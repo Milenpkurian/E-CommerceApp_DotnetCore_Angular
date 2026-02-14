aec653f7efc2740f41b40b79ca9c6631f3551021
Milen <109853036+Milenpkurian@users.noreply.github.com>
2026-02-14 07:46:38 +0000

    filter and sorting

--- skinet/Core/Interfaces/IProductRepository.cs
+++ skinet/Core/Interfaces/IProductRepository.cs
@@ -5,11 +5,13 @@ namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-    Task<IReadOnlyList<Product>> GetProductsAsync();
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
     Task<Product?> GetProductByIdAsync(int id);
     void AddProduct(Product product);
     void UpdateProduct(Product product);
     void DeleteProduct(Product product);
     bool ProductExists(int id);
+    Task<IReadOnlyList<String>> GetBrandsAsync();
+    Task<IReadOnlyList<String>> GetTypesAsync();
     Task<bool> SaveChangesAsync();
 }
